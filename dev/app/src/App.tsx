fe42fc450f4be356d28f4f883ffac69429540735
samkaj <kajavasamuel@gmail.com>
2022-04-05 20:31:41 +0200

    Test suite: prepare test suite for CI

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './tailwind.css';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-5xl">
-        Hello, World!
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className='text-5xl'>Hello, World!</h1>
+            <p>foo bar</p>
+        </div>
+    );
 }
 
 export default App;
