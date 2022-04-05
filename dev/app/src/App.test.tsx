6bcd961b31452070ff0f0ca7b6625855389852c9
samkaj <kajavasamuel@gmail.com>
2022-04-05 20:31:41 +0200

    Test suite: prepare test suite for CI

--- dev/app/src/App.test.tsx
+++ dev/app/src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Hello, World!', () => {
+    render(<App />);
+    const helloWorld = screen.getByText(/Hello, World!/i);
+    expect(helloWorld).toBeInTheDocument();
+});
+
+test('renders foo bar', () => {
+    render(<App />);
+    const foo = screen.getByText(/foo bar/i);
+    expect(foo).toBeInTheDocument();
 });
