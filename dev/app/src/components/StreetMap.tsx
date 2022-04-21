7289dac6319082098f0d657581a7e200c7644cca
Hugo Mårdbrink <hugo@mardbrink.se>
2022-04-21 17:43:05 +0200

    Implemented markers for the 2 users, interable and logs coords

--- dev/app/src/components/StreetMap.tsx
+++ dev/app/src/components/StreetMap.tsx
@@ -1,15 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl, { Map } from 'mapbox-gl';
+import mapboxgl, {Map, Marker} from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltam9obiIsImEiOiJjbDFxNGRwajYwN2lrM2xudWl4dzloaXo4In0.ul3d8p97UuUMYOLADmbNEg';
 
 function StreetMap() {
+    const markerOffset = 0.015;
+    const fstColor = "#ef3038";
+    const sndColor = "#1f75fe"
+
     const mapContainer = useRef<any>(null);
     const map = useRef<Map|null>(null);
+
     const [lng, setLng] = useState(11.97);
     const [lat, setLat] = useState(57.71);
-    const [zoom, setZoom] = useState(9);
+    const [zoom, setZoom] = useState(12);
 
+    const [fstMarker,setFstMarker] = useState<Marker|null>(null);
+    const [sndMarker,setSndMarker] = useState<Marker|null>(null);
+
+    // Create map
     useEffect(() => {
         if (map.current) return;
         map.current = new mapboxgl.Map({
@@ -20,8 +29,31 @@ function StreetMap() {
         });
     });
 
+    // Create markers
+    useEffect(() => {
+        if (map.current instanceof Map) {
+            if(fstMarker) return;
+            setFstMarker(new mapboxgl.Marker({
+                color: fstColor,
+                draggable: true,
+            })
+                .setLngLat([lng - markerOffset, lat - markerOffset])
+                .addTo(map.current));
+
+            if(sndMarker) return;
+            setSndMarker(new mapboxgl.Marker({
+                color: sndColor,
+                draggable: true,
+            })
+                .setLngLat([lng + markerOffset, lat + markerOffset])
+                .addTo(map.current));
+        }
+    });
+
     return (
-        <div ref={mapContainer} className="map-container w-2/3 h-2/3 text-white" />
+        <div>
+            <div ref={mapContainer} className="map-container w-2/3 h-2/3 text-white"/>
+        </div>
     );
 }
 
