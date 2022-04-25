d9d97903224c3e1a00d6d83109d452deb38efa93
Simon Johnsson <simzter@outlook.com>
2022-04-25 16:07:01 +0200

    URL: update react-router-dom to v6

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { 
-  BrowserRouter,
-  Switch,
-  Route,
- } from 'react-router-dom';
-import Home from './pages/Home'
-import PoolPage from './pages/PoolPage';
-import './tailwind.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import PoolPage from "./pages/PoolPage";
+import "./tailwind.css";
 
+function Wrapper() {
+  return (
+    <div>
+      <h1>wrapper</h1>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/pool" component={ PoolPage }/>
-        <Route path="/" component={ Home }/>
-      </Switch>
+      <Routes>
+        <Route  path="/" element={<Outlet />}>
+          <Route path="" element={<Home />} />
+          <Route path="pool" element={<PoolPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
-export default App
+export default App;
