44073a244df4925f27aa23fa723a05841f6fc1c4
Simon Johnsson <simzter@outlook.com>
2022-04-25 16:09:31 +0200

    URL: remove Outlet component

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import PoolPage from "./pages/PoolPage";
 import "./tailwind.css";
-
-function Wrapper() {
-  return (
-    <div>
-      <h1>wrapper</h1>
-      <Outlet />
-    </div>
-  );
-}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path="/" element={<Outlet />}>
+        <Route path="/">
           <Route path="" element={<Home />} />
           <Route path="pool" element={<PoolPage />} />
         </Route>
