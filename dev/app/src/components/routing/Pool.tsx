1c2f7bfa75988f6977956d49727edf3b06f85200
Simon Johnsson <simzter@outlook.com>
2022-04-25 17:13:49 +0200

    URL: add query to pool

--- dev/app/src/components/routing/Pool.tsx
+++ dev/app/src/components/routing/Pool.tsx
@@ -1,3 +1,7 @@
-export default function Pool() {
-    return <h2>Foo Bar</h2>
+export default function Pool({ name }: { name: string | null }) {
+  return (
+    <div>
+      <h2>{name} wants you to meetup!</h2>
+    </div>
+  );
 }
--- dev/app/src/pages/PoolPage.tsx
+++ dev/app/src/pages/PoolPage.tsx
@@ -1,7 +1,14 @@
 import Pool from "../components/routing/Pool";
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 export default function PoolPage() {
-    return (
-        <Pool />
-    )
-} 
+  let query = useQuery();
+  return <Pool name={query.get("name")} />;
+}
