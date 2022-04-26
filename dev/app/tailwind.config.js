aeba0f9e24aa1959a6f55ae838121043dd66da2d
samkaj <kajavasamuel@gmail.com>
2022-04-26 16:08:15 +0200

    Tailwind: add background properties to correctly position icons

--- dev/app/tailwind.config.js
+++ dev/app/tailwind.config.js
@@ -1,14 +1,25 @@
 module.exports = {
-  content: [
-      "./public/index.html",
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      height: {
-        '400px': '400px',
-      }
+    content: ["./public/index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            height: {
+                "400px": "400px",
+                "input-height": "25px",
+            },
+            width: {
+                "box-width": "350px",
+            },
+            backgroundImage: {
+                "search-icon": "url('./assets/search.svg')",
+                "expand-icon": "url('./assets/expand.svg')",
+            },
+            backgroundSize: {
+                sm: "1.4rem",
+            },
+            backgroundPosition: {
+                "left-sm": "center left 0.5rem",
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/typography'),],
-}
+    plugins: [require("@tailwindcss/typography")],
+};
