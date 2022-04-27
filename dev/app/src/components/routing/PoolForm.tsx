2936089f6ce80c6f67cf7e3f56ecdac54711753f
Simon Johnsson <simzter@outlook.com>
2022-04-27 11:43:51 +0200

    URL: add basic poolform

--- dev/app/src/components/routing/PoolForm.tsx
+++ dev/app/src/components/routing/PoolForm.tsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from "react";
 
 export default function PoolForm() {
-    const [name, setName] = useState("")
+  const [name, setName] = useState("");
+  const [lat, setLat] = useState(100)
+  const [long, setLong] = useState(100)
 
-    const handleChange = (event:Event) => {
-        setName((event.target as HTMLTextAreaElement)?.value)
-    }
 
-    return (
-        <div>
-            <form>
-                <label>Enter your name:
-                    <input
-                    type="text"
-                    value={name}
-                    onChange={(e:any) => handleChange(e) }
-                    />
-                </label>
-            </form>
-        </div>
-    )
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    const search = `${window.location.host}/pool?name=${name}&long=${long}&lat=${lat}`
+    alert(`URL: ${search}`)
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label> Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e: any) => setName(e.target.value)}
+          />
+        </label>
+        <label> Enter your destination longitude:
+          <input
+            type="number"
+            value={long}
+            onChange={(e: any) => setLong(e.target.value)}
+          />
+          <label> Enter your destination latitude
+              <input
+                type="number"
+                value={lat}
+                onChange={(e: any) => setLat(e.target.value)}
+              />
+          </label>
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
