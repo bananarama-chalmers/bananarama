f4fc35efdde6561a91e474b94bea127cb8aec841
Axel Söderberg <karlaxel01@hotmail.com>
2022-04-29 09:36:17 +0200

    Invite: added button to copy invitation to clipboard

--- dev/app/src/components/Share.tsx
+++ dev/app/src/components/Share.tsx
@@ -4,9 +4,11 @@ import React from "react";
 function Share({location}: {location: string}) {
     return (
         <div>
+            <button onClick={() => {navigator.clipboard.writeText(generateInvite(location))}}> Copy invite </button>
             <a href={generateMailRef(location)}> Invite by email</a>
         </div>
     );
+
 }
 
 function generateMailRef(location: string) {
@@ -14,6 +16,10 @@ function generateMailRef(location: string) {
     return "mailto:?to=&subject=Pool%20with%20me!&body=Lets%20go%20to%20" + location
         + "%20together!%20Send%20your%20location%20to%20me%20and%20'I’ll%20find%20a%20meeting%20point%20%0A%0A"
 
+}
+
+function generateInvite(location: string) {
+    return "Lets go to " + location + " together! Send your location to me and 'I’ll find a meeting point";
 }
 
 export default Share;
