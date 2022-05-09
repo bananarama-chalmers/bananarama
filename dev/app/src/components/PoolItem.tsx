6aaa601da89b138b14bd17ded85a7ad9d3b216af
samkaj <kajavasamuel@gmail.com>
2022-05-09 15:43:17 +0200

    UI: add dynamic coloring of Poolers

--- dev/app/src/components/PoolItem.tsx
+++ dev/app/src/components/PoolItem.tsx
@@ -8,10 +8,15 @@ type PoolItemProps = {
 
 export const PoolItem = ({ poolerName, travelType, color }: PoolItemProps) => {
     const travelTypes = ["Car", "Walk", "Bike", "Bus"];
-
+    const border = "border-" + color;
     return (
         <li className="text-slate-900">
-            <div className={"block w-3 border-8 rounded-full mt-1 mr-2 float-left border-"+color}></div>
+            <div
+                className={
+                    "block w-3 border-8 rounded-full mt-1 mr-2 float-left " +
+                    border
+                }
+            ></div>
             <p className="float-left">{poolerName}</p>
             <div className="block border-slate-500 w-3 border-8 rounded-full mt-1 ml-2 float-right"></div>
             <p className="float-right">{travelTypes[travelType]}</p>
