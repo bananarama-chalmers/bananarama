bfcbb43a5d7094fa0c9a6d8bf9a0e71838af04d6
kajava <kajavasamuel@gmail.com>
2022-05-09 09:57:53 +0200

    Merge with main

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import FormPage from "./pages/FormPage";
@@ -18,31 +17,5 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-=======
-import React, {useEffect, useState} from "react";
-import "./tailwind.css";
-import StreetMap from "./components/StreetMap";
-import {PoolWizard} from "./components/PoolWizard";
-import {Coordinate} from "./model/generate-map";
-
-const App = () => {
-    const [startLocation, setStartLocation] = useState<Coordinate>();
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((response:GeolocationPosition) => {
-            setStartLocation({lng:response.coords.longitude, lat:response.coords.latitude});
-        },
-        (e:any) => {
-            setStartLocation({lng:11.946472,lat:57.698864});
-        })
-    },[])
-
-    return (
-        <div className="w-screen h-screen">
-            {startLocation && <StreetMap lat={startLocation.lat} lng={startLocation.lng}/>}
-            <PoolWizard />
-        </div>
-    );
->>>>>>> 3031504ffeed5f5bab363867ca504cda60d33027
 }
 export default App;
