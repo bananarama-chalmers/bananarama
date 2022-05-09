77170d5c1ffa60891be1ad617c0b4c10a95fec03
Simon Johnsson <simzter@outlook.com>
2022-05-09 15:23:46 +0200

    Load URL: rebase from main

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -1,6 +1,6 @@
 import "./tailwind.css";
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route }  from "react-router-dom";
 
 import StreetMapView from "./components/StreetMapView";
 import { PoolWizard } from "./components/PoolWizard";
@@ -17,6 +17,8 @@ function App() {
     const [startLocation, setStartLocation] = useState<Coordinate>();
     const [poolers] = useState<Array<Pooler>>(new Array<Pooler>());
     const [destination, setDestination] = useState<Coordinate>({ lng: 11.946472, lat: 57.698864 });
+
+
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
