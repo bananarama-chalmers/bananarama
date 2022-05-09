8df14a7cb35904047aed187243c0c2d0cca70c1f
samkaj <kajavasamuel@gmail.com>
2022-05-09 15:43:38 +0200

    Tests: formatting

--- dev/app/src/test/complex-polygon.test.ts
+++ dev/app/src/test/complex-polygon.test.ts
@@ -1,9 +1,11 @@
-import { Coordinate } from '../components/PoolWizard';
-import {ComplexPolygon} from "../model/complex-polygon";
+import { Coordinate } from "../types/types";
+import { ComplexPolygon } from "../model/complex-polygon";
 
 test("dummy test for isPointInside", () => {
-  let poly = new ComplexPolygon();
-  expect(poly.isPointInside({lat: 1337, lng: 1337} as Coordinate)).toBe(false);
+    let poly = new ComplexPolygon();
+    expect(poly.isPointInside({ lat: 1337, lng: 1337 } as Coordinate)).toBe(
+        false
+    );
 });
 
-export {}
+export {};
