9e905e7a4517036f0125e6e3c9cab3950f8856f4
Simon Johnsson <simzter@outlook.com>
2022-05-09 15:26:08 +0200

    Load URL: change setStartLocation to pos instead of default

--- dev/app/src/components/StreetMap.tsx
+++ dev/app/src/components/StreetMap.tsx
@@ -46,7 +46,7 @@ const StreetMap = () => {
                 });
             },
             (e: any) => {
-                setStartLocation(defaultCoord);
+                setStartLocation(pos);
             }
         );
     }, []);
