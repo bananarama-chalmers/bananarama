afdb3870d95e12ccc69abbc60434f039aa71a124
kajava <kajavasamuel@gmail.com>
2022-05-11 14:07:22 +0200

    SearchBox: add search functionality from model

--- dev/app/src/components/SearchBox.tsx
+++ dev/app/src/components/SearchBox.tsx
@@ -1,38 +1,67 @@
 import React, { useEffect, useState } from "react";
+import { searchCompleter } from "../model/search-completer";
 
 type SearchBoxProps = {
-    callback: Function;
     placeholder: string;
 };
 
-export const SearchBox = ({ callback, placeholder }: SearchBoxProps) => {
-    const [isExpanded, setIsExpanded] = useState(true);
+export const SearchBox = ({ placeholder }: SearchBoxProps) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [searchResults, setSearchResults] = useState<Array<string>>([]);
+    const [selectedResult, setSelectedResult] = useState<string>("");
 
-    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const searchString: string = e.target.value;
-        callback(searchString);
+    const handleSearch = async (query: string) => {
+        const sc: searchCompleter = new searchCompleter();
+        sc.suggestions(query).then((r: Array<string>) => {
+            setSearchResults(r);
+        });
     };
 
-    const handleBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setIsExpanded(true);
-        console.log(isExpanded);
+    const handleSelect = (e: React.MouseEvent<HTMLLIElement>) => {
+        let s = e.currentTarget.innerText;
+        handleSearch(s);
+        setSelectedResult(s);
     };
 
-    const handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setIsExpanded(false);
-        console.log(isExpanded);
-    };
-
-    return (
-        <div>
+    const searchInput = () => {
+        return (
             <input
                 className="w-full bg-search-icon bg-sm bg-no-repeat p-1 pl-9 bg-left-sm bg-white rounded-md border h-10 outline-slate-200"
                 type="text"
                 placeholder={placeholder}
-                onChange={handleOnChange}
-                onBlur={handleBlur}
-                onFocus={handleFocus}
+                value={selectedResult}
+                onChange={(e) => handleSearch(e.currentTarget.value)}
+                onBlur={() => setIsExpanded(false)}
+                onFocus={() => setIsExpanded(true)}
             />
+        );
+    };
+
+    const searchResult = (result: string, key: number) => {
+        return (
+            <li
+                key={key}
+                className="w-full block bg-auto hover:bg-slate-100 p-1 hover:cursor-pointer hover:text-blue-400 hover:underline"
+                onMouseDown={(e) => handleSelect(e)}
+            >
+                {result}
+            </li>
+        );
+    };
+
+    const searchResultBox = () => {
+        return (
+            <ul className="absolute bg-white font-normal z-20 w-56 shadow-md">
+                {searchResults.map((result, key) => searchResult(result, key))}
+            </ul>
+        );
+    };
+
+    return isExpanded ? (
+        <div>
+            {searchInput()} {searchResultBox()}
         </div>
+    ) : (
+        <div>{searchInput()}</div>
     );
 };
