ca6d9737bed8251108ea7f5cef5c04f1ecabbd34
kajava <kajavasamuel@gmail.com>
2022-05-12 17:31:29 +0200

    Navigation: increase z-index to show navbar always

--- dev/app/src/components/Navigation.tsx
+++ dev/app/src/components/Navigation.tsx
@@ -13,7 +13,7 @@ export const Navigation = () => {
         { url: "/map", key: "map", text: "Map" },
     ];
     return (
-        <nav className="fixed left-0 top-0 w-full px-3 py-2 bg-white border-b border-slate-200 ">
+        <nav className="fixed z-30 left-0 top-0 w-full px-3 py-2 bg-white border-b border-slate-200 ">
             <ul className="list-none">
                 <li className="float-left p-2 font-bold text-2xl text-violet-500 hover:text-violet-600">
                     <Link to="/">{"pooler."}</Link>
