38d2aa0d5c3f39433bfafc7c52593c50fde9aa01
kajava <kajavasamuel@gmail.com>
2022-05-12 17:48:48 +0200

    Dark Theme: add dark theme support

--- dev/app/src/components/Navigation.tsx
+++ dev/app/src/components/Navigation.tsx
@@ -13,13 +13,13 @@ export const Navigation = () => {
         { url: "/map", key: "map", text: "Map" },
     ];
     return (
-        <nav className="fixed z-30 left-0 top-0 w-full px-3 py-2 bg-white border-b border-slate-200 ">
+        <nav className="fixed z-30 left-0 top-0 w-full px-3 py-2 dark:bg-neutral-900 dark:text-white bg-white border-b border-slate-200 ">
             <ul className="list-none">
-                <li className="float-left p-2 font-bold text-2xl text-violet-500 hover:text-violet-600">
+                <li className="float-left p-2 font-bold text-2xl text-violet-500 hover:text-violet-600 dark:text-white dark:hover:text-orange-400">
                     <Link to="/">{"pooler."}</Link>
                 </li>
                 {navItems.map((item) => (
-                    <li className="float-right p-2 font-medium my-auto leading-loose hover:text-blue-500">
+                    <li className="float-right p-2 font-medium my-auto leading-loose dark:text-white dark:hover:text-orange-400  hover:text-blue-500">
                         <Link to={item.url}>{item.text}</Link>
                     </li>
                 ))}
