2442a53c11f04884378811fdbd3f0d598af15c0e
kajava <kajavasamuel@gmail.com>
2022-05-12 17:15:11 +0200

    Navigation: add navbar to App

--- dev/app/src/App.tsx
+++ dev/app/src/App.tsx
@@ -3,16 +3,26 @@ import "./tailwind.css";
 import StreetMapView from "./components/StreetMapView";
 import { PoolWizard } from "./components/PoolWizard";
 import { LandingPage } from "./pages/LandingPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route
+                    path="/"
+                    element={
+                        <div>
+                            <Navigation />
+                            <LandingPage />
+                        </div>
+                    }
+                />
                 <Route
                     path="/map"
                     element={
                         <div className="w-screen h-screen">
+                            <Navigation />
                             <StreetMapView />
                             <PoolWizard />
                         </div>
