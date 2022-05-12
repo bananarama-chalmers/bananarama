3431c2d8392fe42765c7a05a2a2f1297fedf2c6d
kajava <kajavasamuel@gmail.com>
2022-05-12 11:59:16 +0200

    Redundancy: remove unused variable declaration

--- dev/app/src/model/search-completer.ts
+++ dev/app/src/model/search-completer.ts
@@ -7,26 +7,29 @@ mapboxgl.accessToken =
 //input partial searchparameter as a string
 //return a promise with and array of suggested adresses
 //see geo-coding forwardGeocoding for how to use the response
-export class searchCompleter{
-    public async suggestions(partialSearch:string):Promise<Array<string>>{
+export class searchCompleter {
+    public async suggestions(partialSearch: string): Promise<Array<string>> {
         //return string
         let returnString: Array<string> = [];
         //encode the adress
         let encodedAdress = encodeURI(partialSearch);
 
-        const promise = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-            encodedAdress +
-            '.json?proximity=ip&access_token=' +
-            mapboxgl.accessToken).then((response:any) => {
-                if(response.data.features.length < 1){
+        await axios
+            .get(
+                "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+                    encodedAdress +
+                    ".json?proximity=ip&access_token=" +
+                    mapboxgl.accessToken
+            )
+            .then((response: any) => {
+                if (response.data.features.length < 1) {
                     returnString.push("Adress not found");
-                }else{
-                    for(let i = 0; i < 5; i++){
+                } else {
+                    for (let i = 0; i < 5; i++) {
                         returnString.push(response.data.features[i].place_name);
                     }
                 }
-
-        });
+            });
         return returnString;
     }
 }
