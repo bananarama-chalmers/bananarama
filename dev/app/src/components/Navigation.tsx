8d2282c79f3c1a7823c25c29f8c01b17d1a69d96
kajava <kajavasamuel@gmail.com>
2022-05-13 14:46:27 +0200

    Nav: add keys to all list objects

--- dev/app/src/components/Navigation.tsx
+++ dev/app/src/components/Navigation.tsx
@@ -15,11 +15,17 @@ export const Navigation = () => {
     return (
         <nav className="fixed z-30 left-0 top-0 w-full px-3 py-2 bg-white border-b border-slate-200 ">
             <ul className="list-none">
-                <li className="float-left p-2 font-bold text-2xl text-violet-500 hover:text-violet-600">
+                <li
+                    key={"home logo"}
+                    className="float-left p-2 font-bold text-2xl text-violet-500 hover:text-violet-600"
+                >
                     <Link to="/">{"pooler."}</Link>
                 </li>
                 {navItems.map((item) => (
-                    <li className="float-right p-2 font-medium my-auto leading-loose hover:text-blue-500">
+                    <li
+                        key={item.key}
+                        className="float-right p-2 font-medium my-auto leading-loose hover:text-blue-500"
+                    >
                         <Link to={item.url}>{item.text}</Link>
                     </li>
                 ))}
