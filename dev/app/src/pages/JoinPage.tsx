9e022b297d830eabed57d97d46e897392b0b6a09
Simon Johnsson <simzter@outlook.com>
2022-05-18 16:34:48 +0200

    Join Pool: add input prompt and change text size

--- dev/app/src/pages/JoinPage.tsx
+++ dev/app/src/pages/JoinPage.tsx
@@ -2,32 +2,36 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const JoinPage = () => {
-    const [joinLink, setJoinLink] = useState("")
+    const [joinLink, setJoinLink] = useState("");
 
     let navigate = useNavigate();
     const routeChange = () => {
         let path = "../map/" + joinLink;
         navigate(path);
-    }
-
-    const handleSubmit = (event:any) => {
-        event.preventDefault()
-        console.log(joinLink)
-        routeChange()
     };
 
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(joinLink);
+        routeChange();
+    };
 
     return (
         <div className="content-center bg-white dark:bg-neutral-900 w-screen h-screen py-40 px-10">
             <h1 className="leading-none my-4 font-bold dark:text-white text-7xl text-center">
                 {"Join a pool with your link here"}
             </h1>
+            <p className="text-xl mb-4 text-center dark:text-white">
+                {
+                    "You should have received a link from your intended car pool host"
+                }
+            </p>
             <form onSubmit={handleSubmit}>
                 <div className="flex items-center border-b dark:border-violet-600 border-blue-500 py-2">
                     <label>
                         <input
                             type="text"
-                            className="appearance-none bg-transparent border-none w-full text-gray-700 dark:text-white text-4xl mr-3 py-1 px-2 focus:outline-none"
+                            className="appearance-none bg-transparent border-none w-full text-gray-700 dark:text-white text-3xl mr-3 py-1 px-2 focus:outline-none"
                             placeholder="Enter your link"
                             value={joinLink}
                             onChange={(e) => setJoinLink(e.target.value)}
